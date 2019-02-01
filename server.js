7142178f3eed60fedcc4cb5f6bd2c469e34a916f
Torben Jepsen <torbenjepsen@MSP22TJJ010322.local>
2019-02-01 11:13:13 -0600

    added open weather api

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const apiKey = 'dcc8baa793b11664073b856ea7c00510';
+
+// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    res.render('index', {weather: null, error: null});
+});
+
+app.post('/', function (req, res) {
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+    request(url, function (err, response, body) {
+        console.log(err)
+        if (err) {
+            res.render('index', { weather: null, error: 'Error, please try again' });
+        } else {
+            let weather = JSON.parse(body)
+            if (weather.main == undefined) {
+                res.render('index', { weather: null, error: 'Error, please try again' });
+            } else {
+                let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+                res.render('index', { weather: weatherText, error: null });
+            }
+        }
+    });
+})
+
+app.listen(PORT, function () {
+    console.log(`Turnin and burnin on port ${PORT}!`);
+});
